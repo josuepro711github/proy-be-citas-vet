3475f04f17da442729cc30ed686097aafc498215
Henry Vidal Fernandez <henry145123@gmail.com>
2023-09-20 01:12:43 -0500

    commit cliente : nombre_gaaaa

--- src/main/java/com/utp/edu/pe/repository/DoctorRepository.java
+++ src/main/java/com/utp/edu/pe/repository/DoctorRepository.java
@@ -2,6 +2,8 @@ package com.utp.edu.pe.repository;
 
 import com.utp.edu.pe.model.Doctor;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface DoctorRepository extends JpaRepository<Doctor,Integer> {
 }
--- src/main/java/com/utp/edu/pe/repository/RolRepository.java
+++ src/main/java/com/utp/edu/pe/repository/RolRepository.java
@@ -2,6 +2,8 @@ package com.utp.edu.pe.repository;
 
 import com.utp.edu.pe.model.Rol;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface RolRepository extends JpaRepository<Rol,Integer> {
 }
