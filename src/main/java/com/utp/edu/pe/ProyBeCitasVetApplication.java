a03919c446d6283f2bf9391d981712522f4426bd
Josuepro711 <josuepro711@hotmail.com>
2023-09-19 23:24:51 -0500

    subida de fuentes

---
+++ src/main/java/com/utp/edu/pe/ProyBeCitasVetApplication.java
@@ -0,0 +1,13 @@
+package com.utp.edu.pe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyBeCitasVetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyBeCitasVetApplication.class, args);
+	}
+
+}
