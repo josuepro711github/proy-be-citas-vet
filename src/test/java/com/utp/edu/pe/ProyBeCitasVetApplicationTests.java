4369d60760feb5ef4a9e9c155efbb407eaa72fd8
Josuepro711 <josuepro711@hotmail.com>
2023-09-19 23:30:50 -0500

    subida de fuentes

---
+++ src/test/java/com/utp/edu/pe/ProyBeCitasVetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.utp.edu.pe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyBeCitasVetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
